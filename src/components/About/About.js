adb23da14ac1dab2d58353c3af32d0f2a00cc910
Fronikuniu <Fronikuniu@gmail.com>
2021-10-18 19:20:41 +0200

    Change About view component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,32 +1,29 @@
-// import '../../styles/components/About/about.css';
 import dragon from '../../images/dragon.png';
-import Shape from '../Decorations/Shape';
 
 function About() {
   return (
-    <div className="about__container">
-      <Shape />
+    <section className="about">
       <div className="container">
         <h1 className="header">About</h1>
         <h2 className="header">Lavarc</h2>
-        <section className="about">
-          <div className="about__info">
-            <div className="about__info-text">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro odit corporis ipsam similique. Ut maxime dolorem perferendis aliquam distinctio autem eius vitae ratione. Excepturi,
-                illum saepe. Officiis nobis explicabo ducimus sit, vero vel in, voluptate eligendi cumque laboriosam animi, aliquid excepturi illo ab modi eos ratione ipsum deleniti eveniet suscipit
-                quia magnam totam. Neque, suscipit obcaecati! Iure doloribus repudiandae itaque voluptatem non quibusdam quia, eius beatae eum aspernatur debitis aut distinctio, cupiditate atque quam
-                laudantium voluptatibus odio architecto saepe magnam? Culpa optio, odio deserunt consectetur porro nesciunt ea at doloremque sint, non neque perspiciatis sed! Possimus molestiae
-                obcaecati veritatis minus?
-              </p>
-            </div>
-            <div className="about__info-img">
-              <img src={dragon} alt="Dragon by Fronikuniu" className="dragon" />
-            </div>
+
+        <div className="about__info">
+          <div className="about__info-text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro odit corporis ipsam similique. Ut maxime dolorem perferendis aliquam distinctio autem eius vitae ratione. Excepturi, illum
+              saepe. Officiis nobis explicabo ducimus sit, vero vel in, voluptate eligendi cumque laboriosam animi, aliquid excepturi illo ab modi eos ratione ipsum deleniti eveniet suscipit quia
+              magnam totam. Neque, suscipit obcaecati! Iure doloribus repudiandae itaque voluptatem non quibusdam quia, eius beatae eum aspernatur debitis aut distinctio, cupiditate atque quam
+              laudantium voluptatibus odio architecto saepe magnam? Culpa optio, odio deserunt consectetur porro nesciunt ea at doloremque sint, non neque perspiciatis sed! Possimus molestiae
+              obcaecati veritatis minus?
+            </p>
           </div>
-        </section>
+
+          <div className="about__info-img">
+            <img src={dragon} alt="Dragon by Fronikuniu" className="dragon" />
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
