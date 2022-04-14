380ef7368bd32a9e2b83d00ee183bcc609014263
Fronikuniu <Fronikuniu@gmail.com>
2022-04-14 14:47:49 +0200

    Add RWD to shop

--- src/components/Shop/ShopCartList.tsx
+++ src/components/Shop/ShopCartList.tsx
@@ -23,20 +23,22 @@ function ShopCartList({ cart, removeFromCart, lenght }: ShopCartListProps) {
                 <IoClose className="pointer" onClick={() => removeFromCart(item)} />
               </div>
               <img src={item.imageSrc} alt={item.title} className="image" />
+            </div>
+            <div className="bottom-side">
               <div className="text">
                 <p className="title">{item.title}</p>
                 <p className="builder">{item.builder}</p>
               </div>
-            </div>
-            <div className="cost">
-              {item.sale ? (
-                <div className="sale">
-                  <p className="sale-price">{item.price}$</p>
-                  <p className="sale-sale">{item.sale}$</p>
-                </div>
-              ) : (
-                <p className="price">{item.price}$</p>
-              )}
+              <div className="cost">
+                {item.sale ? (
+                  <div className="sale">
+                    <p className="sale-price">{item.price}$</p>
+                    <p className="sale-sale">{item.sale}$</p>
+                  </div>
+                ) : (
+                  <p className="price">{item.price}$</p>
+                )}
+              </div>
             </div>
           </div>
         ))
